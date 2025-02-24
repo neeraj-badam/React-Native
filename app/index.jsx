ff0f5a45761c64f010ef09fbf64f4ae7dd5e4bbd
neeraj-badam <neeraj.badam@gmail.com>
2025-02-24 15:23:12 -0500

    FlatList

--- app/index.jsx
+++ app/index.jsx
@@ -10,6 +10,11 @@ const app = () => {
           resizeMode='cover'
           style={styles.image}>
         <Text style={styles.title}>Coffee Shop</Text>
+        <Link href={'/menu'} style={{ marginHorizontal: 'auto' }} asChild>
+          <Pressable style= { styles.button }>
+            <Text style={styles.buttonText}>Menu</Text>
+          </Pressable>
+        </Link>
         <Link href={'/contact'} style={{ marginHorizontal: 'auto' }} asChild>
           <Pressable style= { styles.button }>
             <Text style={styles.buttonText}>Contact us</Text>
@@ -53,10 +58,12 @@ const styles = StyleSheet.create({
   },
   button: {
     height: 60,
+    width: 150,
     borderRadius: 20,
     backgroundColor: 'rgba(0,0,0,0.75)',
     padding: 6,
     justifyContent: 'center',
+    marginBottom: 50,
   },
   buttonText: {
     color: 'white',
